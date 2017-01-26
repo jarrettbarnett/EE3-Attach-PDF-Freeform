cc0582d793f0a75406193b997fe2696d8961a0ff
Jarrett Barnett <git@jarrettbarnett.com>
2017-01-26 11:47:01 -0800

    Updated version information

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ if (! defined('JB_ATTACHPDF_VERSION'))
     define('JB_ATTACHPDF_NAME', 'JB Attach PDF');
     define('JB_ATTACHPDF_CLASS', 'Jb_attachpdf');
     define('JB_ATTACHPDF_NAMESPACE',  'Jb\Attachpdf');
-    define('JB_ATTACHPDF_VERSION',  '1.1.1');
+    define('JB_ATTACHPDF_VERSION',  '1.0.0');
     define('JB_ATTACHPDF_AUTHOR',  'Jarrett Barnett');
     define('JB_ATTACHPDF_AUTHOR_URL',  'http://jarrettbarnett.com');
     define('JB_ATTACHPDF_DESC',  '');
-    define('JB_ATTACHPDF_DOCS',  'http://jarrettbarnett.com/docs/jb_attachpdf');
+    define('JB_ATTACHPDF_DOCS',  'https://gist.github.com/jarrettbarnett/1a682e489e486e92f676b16a4d3a9607');
     define('JB_ATTACHPDF_SETTINGSEXIST',  false);
 }
